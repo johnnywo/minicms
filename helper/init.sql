02aa3a8469b0bcd101f372976b906b26ab0cafbf
johnnywo <office@zeero.at>
2016-05-11 13:56:19 +0200

    init.sql database column

--- helper/init.sql
+++ helper/init.sql
@@ -16,12 +16,12 @@ CREATE TABLE page_has_language
     language_idlanguage INT(11) NOT NULL,
     pl_title VARCHAR(45),
     pl_meta_description VARCHAR(255),
-    pl_url VARCHAR(55),
     pl_h1 VARCHAR(45),
     pl_htmltext TEXT,
     pl_menu_link_title VARCHAR(55),
     pl_menu_link_main_menu TINYINT(1) DEFAULT '0',
     pl_menu_link_footer_menu TINYINT(1) DEFAULT '0',
+    pl_image VARCHAR(100),
     CONSTRAINT `PRIMARY` PRIMARY KEY (page_idpage, language_idlanguage),
     CONSTRAINT fk_content_has_language_content FOREIGN KEY (page_idpage) REFERENCES page (idpage),
     CONSTRAINT fk_content_has_language_language1 FOREIGN KEY (language_idlanguage) REFERENCES language (idlanguage)
